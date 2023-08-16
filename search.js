3214744a29aa2c8af7230ae4fc78cea851985c8d
izbuzz <ivzhang@stu.allstarcode.org>
2023-08-16 13:13:10 -0400

    Rename main.js to gallery.js and import search.js for html files

--- search.js
+++ search.js
@@ -1,7 +1,9 @@
 const searchbox = document.getElementById("searchbox");
 const input = document.getElementById("search-input");
 
+console.log("test");
 searchbox.addEventListener("submit", () => {
+  console.log("test");
   location.href = "search.html";
   return false;
 });
