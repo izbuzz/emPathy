cdf5ee1eb897eeb0022a23657ad9ec2e99710a32
izbuzz <ivzhang@stu.allstarcode.org>
2023-08-17 15:04:15 -0400

    Remove console.log in search.js

--- search.js
+++ search.js
@@ -1,9 +1,7 @@
 const searchbox = document.getElementById("searchbox");
 const input = document.getElementById("search-input");
 
-console.log("test");
 searchbox.addEventListener("submit", (event) => {
   event.preventDefault();
   location.href = "search.html";
-  return false;
 });
